c1534a9d5adc82465d3c2306c00226fab0ee55f8
Ta-Yun Yang <tayuny@cs.uchicago.edu>
2020-06-14 08:36:23 -0500

    adding sh files to get data files for the new created directory data

---
+++ data/get_full_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://drive.google.com/file/d/13X1AjjaU4WZq2YImDrwtHai01Vrk0NV2/view?usp=sharing -O USvideos.csv
+wget https://drive.google.com/file/d/1NwBXL6e4F6n1nKnTm5cnPpZ76ktZMs4e/view?usp=sharing -O US_category_id.json
+wget https://drive.google.com/file/d/1qp4cKzYLvo3azlGM162Dc8qp80vQfwPJ/view?usp=sharing -O unavailable.txt
+wget https://drive.google.com/file/d/17dsjzcQfTNDsNl_UGy7ABivpVGUVeY6_/view?usp=sharing -O transcripts.txt
